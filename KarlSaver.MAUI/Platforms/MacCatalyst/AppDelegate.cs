e1a4e10b573fa2fd0af06b970ab400915eeb03cb
Heitor Correa <heitor.silva28@gmail.com>
2024-05-27 16:32:17 -0300

    Bunch of stuff and start of MAUI app

---
+++ KarlSaver.MAUI/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace KarlSaver.MAUI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
